e58f66e4596b63730b844797d58f6fb7abde8145
Riley <riley.childs@yahoo.com>
2022-05-14 10:03:31 -0400

    Fixed overflowing x-axis elements

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -24,3 +24,13 @@ let toggleMenu = () => {
       }
 };
 
+//Use to dedect offset elements
+/* var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+); */
